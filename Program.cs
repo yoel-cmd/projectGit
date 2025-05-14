30cefd8385fa804fc10e45514f7e36b2e2925362
Inon Guetta <inon16guetta@gmail.com>
2025-05-14 15:05:29 +0300

    the_base_of_my_functions

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,24 @@ namespace projectGit
 {
     internal class Program
     {
+
+        
+        List<string> problem_words = new List<string> { "לוחם", "נח'בה","פצצה","טיל","סוד"};
+        static string scans_problematic_words(string str, List<string> problrm_words)
+        {
+            
+            return str;
+
+        }
+
+
         static void Main(string[] args)
+
+
+
+
+
+
         {
         }
     }
